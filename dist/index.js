51424e5485eff3c827355070c511ca521705ccd7
Niklas Lindgren <niklas@musicglue.com>
2023-01-31 11:33:06 +0000

    chore: build

--- dist/index.js
+++ dist/index.js
@@ -9572,6 +9572,7 @@ const ccFormat = /^(chore|docs|feat|fix|refactor|style|test)(\([^)]+\))?: .+$/;
                 repo: repo.name,
                 pull_number: pr.number,
             });
+            let hasErrors = false;
             // Using .forEach instead of .some/.all so that all commits are validated in one go, instead of
             // making it a game of whack-a-mole
             commits.forEach(({ commit: { message }, sha }) => {
@@ -9579,12 +9580,17 @@ const ccFormat = /^(chore|docs|feat|fix|refactor|style|test)(\([^)]+\))?: .+$/;
                 const subjectLine = message.split("\n\n").shift().replace(/\n/g, "");
                 core.debug(`checking: "${subjectLine}"`);
                 if (subjectLine.length > maxSubjectLen) {
+                    hasErrors = true;
                     core.error(`error: length sha=${sha} subject="${subjectLine}"`);
                 }
                 if (!ccFormat.test(subjectLine)) {
+                    hasErrors = true;
                     core.error(`error: format sha=${sha} subject="${subjectLine}"`);
                 }
             });
+            if (hasErrors) {
+                core.setFailed(`one or more commit messages violates commit conventions. see the linter job output for more details.`);
+            }
         }
         catch (error) {
             core.setFailed(error.message);
